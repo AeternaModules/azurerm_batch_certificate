d1d8778c0cb8d0d5f705e28372845917f379f72a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,44 +1,39 @@
-output "batch_certificates" {
-  description = "All batch_certificate resources"
-  value       = azurerm_batch_certificate.batch_certificates
-  sensitive   = true
-}
 output "batch_certificates_account_name" {
-  description = "List of account_name values across all batch_certificates"
-  value       = [for k, v in azurerm_batch_certificate.batch_certificates : v.account_name]
+  description = "Map of account_name values across all batch_certificates, keyed the same as var.batch_certificates"
+  value       = { for k, v in azurerm_batch_certificate.batch_certificates : k => v.account_name }
 }
 output "batch_certificates_certificate" {
-  description = "List of certificate values across all batch_certificates"
-  value       = [for k, v in azurerm_batch_certificate.batch_certificates : v.certificate]
+  description = "Map of certificate values across all batch_certificates, keyed the same as var.batch_certificates"
+  value       = { for k, v in azurerm_batch_certificate.batch_certificates : k => v.certificate }
   sensitive   = true
 }
 output "batch_certificates_format" {
-  description = "List of format values across all batch_certificates"
-  value       = [for k, v in azurerm_batch_certificate.batch_certificates : v.format]
+  description = "Map of format values across all batch_certificates, keyed the same as var.batch_certificates"
+  value       = { for k, v in azurerm_batch_certificate.batch_certificates : k => v.format }
 }
 output "batch_certificates_name" {
-  description = "List of name values across all batch_certificates"
-  value       = [for k, v in azurerm_batch_certificate.batch_certificates : v.name]
+  description = "Map of name values across all batch_certificates, keyed the same as var.batch_certificates"
+  value       = { for k, v in azurerm_batch_certificate.batch_certificates : k => v.name }
 }
 output "batch_certificates_password" {
-  description = "List of password values across all batch_certificates"
-  value       = [for k, v in azurerm_batch_certificate.batch_certificates : v.password]
+  description = "Map of password values across all batch_certificates, keyed the same as var.batch_certificates"
+  value       = { for k, v in azurerm_batch_certificate.batch_certificates : k => v.password }
   sensitive   = true
 }
 output "batch_certificates_public_data" {
-  description = "List of public_data values across all batch_certificates"
-  value       = [for k, v in azurerm_batch_certificate.batch_certificates : v.public_data]
+  description = "Map of public_data values across all batch_certificates, keyed the same as var.batch_certificates"
+  value       = { for k, v in azurerm_batch_certificate.batch_certificates : k => v.public_data }
 }
 output "batch_certificates_resource_group_name" {
-  description = "List of resource_group_name values across all batch_certificates"
-  value       = [for k, v in azurerm_batch_certificate.batch_certificates : v.resource_group_name]
+  description = "Map of resource_group_name values across all batch_certificates, keyed the same as var.batch_certificates"
+  value       = { for k, v in azurerm_batch_certificate.batch_certificates : k => v.resource_group_name }
 }
 output "batch_certificates_thumbprint" {
-  description = "List of thumbprint values across all batch_certificates"
-  value       = [for k, v in azurerm_batch_certificate.batch_certificates : v.thumbprint]
+  description = "Map of thumbprint values across all batch_certificates, keyed the same as var.batch_certificates"
+  value       = { for k, v in azurerm_batch_certificate.batch_certificates : k => v.thumbprint }
 }
 output "batch_certificates_thumbprint_algorithm" {
-  description = "List of thumbprint_algorithm values across all batch_certificates"
-  value       = [for k, v in azurerm_batch_certificate.batch_certificates : v.thumbprint_algorithm]
+  description = "Map of thumbprint_algorithm values across all batch_certificates, keyed the same as var.batch_certificates"
+  value       = { for k, v in azurerm_batch_certificate.batch_certificates : k => v.thumbprint_algorithm }
 }
 
