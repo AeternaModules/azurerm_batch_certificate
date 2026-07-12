8449fc72bd1499bc33cae2c1a8181f193cda1587
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:57:53 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,12 +1,22 @@
+data "azurerm_key_vault_secret" "certificate" {
+  for_each     = { for k, v in var.batch_certificates : k => v if v.certificate_key_vault_id != null && v.certificate_key_vault_secret_name != null }
+  name         = each.value.certificate_key_vault_secret_name
+  key_vault_id = each.value.certificate_key_vault_id
+}
+data "azurerm_key_vault_secret" "password" {
+  for_each     = { for k, v in var.batch_certificates : k => v if v.password_key_vault_id != null && v.password_key_vault_secret_name != null }
+  name         = each.value.password_key_vault_secret_name
+  key_vault_id = each.value.password_key_vault_id
+}
 resource "azurerm_batch_certificate" "batch_certificates" {
   for_each = var.batch_certificates
 
   account_name         = each.value.account_name
-  certificate          = each.value.certificate
+  certificate          = each.value.certificate != null ? each.value.certificate : try(data.azurerm_key_vault_secret.certificate[each.key].value, null)
   format               = each.value.format
   resource_group_name  = each.value.resource_group_name
   thumbprint           = each.value.thumbprint
   thumbprint_algorithm = each.value.thumbprint_algorithm
-  password             = each.value.password
+  password             = each.value.password != null ? each.value.password : try(data.azurerm_key_vault_secret.password[each.key].value, null)
 }
 
--- variables.tf
+++ variables.tf
@@ -4,22 +4,30 @@ Map of batch_certificates, attributes below
 Required:
     - account_name
     - certificate
+    - certificate_key_vault_id (alternative to certificate - read from Key Vault instead)
+    - certificate_key_vault_secret_name (alternative to certificate - read from Key Vault instead)
     - format
     - resource_group_name
     - thumbprint
     - thumbprint_algorithm
 Optional:
     - password
+    - password_key_vault_id (alternative to password - read from Key Vault instead)
+    - password_key_vault_secret_name (alternative to password - read from Key Vault instead)
 EOT
 
   type = map(object({
-    account_name         = string
-    certificate          = string
-    format               = string
-    resource_group_name  = string
-    thumbprint           = string
-    thumbprint_algorithm = string
-    password             = optional(string)
+    account_name                      = string
+    certificate                       = string
+    certificate_key_vault_id          = optional(string)
+    certificate_key_vault_secret_name = optional(string)
+    format                            = string
+    resource_group_name               = string
+    thumbprint                        = string
+    thumbprint_algorithm              = string
+    password                          = optional(string)
+    password_key_vault_id             = optional(string)
+    password_key_vault_secret_name    = optional(string)
   }))
 }
 
